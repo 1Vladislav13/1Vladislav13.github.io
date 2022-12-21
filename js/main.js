008dc707557d281254d775c20e794c7a86451790
1Vladislav13 <9093959@mail.ru>
2022-12-21 21:44:15 +0100

    delete formMail

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
 $(function () {
 
-  $("form").submit(function () { //Change
-    var th = $(this);
-    $.ajax({
-      type: "POST",
-      url: "mail.php", //Change
-      data: th.serialize()
-    }).done(function () {
-      alert("Thank you!");
-      setTimeout(function () {
-        // Done Functions
-        th.trigger("reset");
-      }, 1000);
-    });
-    return false;
-  });
-
   const menu = document.querySelector('.menu')
   window.addEventListener('scroll', () => {
     let scrollDistance = window.scrollY
@@ -45,8 +29,6 @@ $(function () {
     $('body,html').animate({ scrollTop: top }, 1500);
   });
 
-
-
 });
 
 var mixer = mixitup('.works__gallery');
@@ -58,6 +40,8 @@ Fancybox.show([
   },
 
 ]);
+
+
 
 
 
