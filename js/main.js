2faa599c74ec004f2ef41e9c6e33e7b99684dd06
1Vladislav13 <9093959@mail.ru>
2022-12-21 21:18:51 +0100

    add mailForm

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(function () {
 
+  $("form").submit(function () { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function () {
+      alert("Thank you!");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
+
   const menu = document.querySelector('.menu')
   window.addEventListener('scroll', () => {
     let scrollDistance = window.scrollY
